b03db4da091092fcf49d85073d1008314952ad47
Gaurav <35771409+gauravgupta45@users.noreply.github.com>
2019-09-23 19:03:47 +0530

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,20 @@
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "******************";
+char ssid[] = "Your Wi-Fi SSID";
+char pass[] = "password";
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth,ssid,pass);
+}
+void loop()
+{
+    Blynk.run();
+}
+
